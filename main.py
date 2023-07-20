6455c39a9ed7e444974c9d178c21dc06d438ae28
RuslanEvent <mayberuslan2@gmail.com>
2023-07-20 17:40:38 +0500

    com

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class Item(BaseModel):
     landmark: str
     price: int
     description: str
-    photos: List[str]
+    photos: List[str] | None = None
 
 
 @app.get('/')
@@ -35,18 +35,33 @@ async def get_r():
 
 @app.post('/')
 async def get(data: Item):
-    collection = []
-    imgs = await download_images(data.photos)
-    msg = text.format(**data.model_dump(exclude={'photo', }))
-    for index, img in enumerate(imgs):
-        collection.append(InputMediaPhoto(watermark(BytesIO(img)), caption=msg if not index else '', parse_mode='html'))
     bot = Bot(conf.TOKEN)
+    if data.photos is not None:
+        collection = []
+        if len(data.photos) > 10:
+            photos = data.photos[:10]
+        else:
+            photos = data.photos
+        imgs = await download_images(photos)
+        msg = text.format(**data.model_dump(exclude={'photo', }))
+        for index, img in enumerate(imgs):
+            collection.append(InputMediaPhoto(watermark(BytesIO(img)), caption=msg if not index else '', parse_mode='html'))
 
-    async with bot:
-        await bot.send_media_group(
-            chat_id=conf.CHANNEL_ID,
-            media=collection,
-            write_timeout=100,
-            read_timeout=30
-        )
+        async with bot:
+            await bot.send_media_group(
+                chat_id=conf.CHANNEL_ID,
+                media=collection,
+                write_timeout=100,
+                read_timeout=30
+            )
+    else:
+        msg = text.format(**data.model_dump(exclude={'photo', }))
+        async with bot:
+            await bot.send_message(
+                chat_id=conf.CHANNEL_ID,
+                text=msg,
+                write_timeout=100,
+                read_timeout=30,
+                parse_mode="html"
+            )
     return {}
