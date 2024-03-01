64a18d036223be1faa79ce7008dbc91cf958de8c
Mirzohid <76915539+mirzox@users.noreply.github.com>
2024-03-01 23:34:28 +0500

    Update message.py
    text structure changed to italic

--- message.py
+++ message.py
@@ -1,10 +1,13 @@
 text = """
-{number_of_rooms}/{floor}/{number_of_floors} {sub_district} {area}m²
-Состояние: {repair}
-Ор-р: {landmark}
-Цена: {price}
-Описание: {description}
-Тел: +998998078071 
-#{number_of_rooms}ком
+<i>{number_of_rooms}/{floor}/{number_of_floors} {sub_district} {area}m²</i>
+
+<i>Состояние: {repair}</i>
+<i>Ор-р: {landmark}</i>
+<i>Описание: {description}</i>
+
+<i>Цена: {price}</i>
+
+<i>Тел: +998998078071 </i>
 <a href='https://t.me/iHometashkent'>Telegram</a> | <a href='https://www.instagram.com/tashkent_realtor'>Instagram</a> | <a href='https://t.me/Majoor_kz'>Написать мне 💬</a>
+#{number_of_rooms}ком
 """
