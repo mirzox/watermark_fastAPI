d88827fa856c8b2b9e71f602b19837828f276dee
Mirzohid <mirzokhid.abdurakhmatov.01@mail.ru>
2024-10-07 21:01:20 +0500

    message changed

--- message.py
+++ message.py
@@ -5,6 +5,7 @@ text = """
 <i>Цена: {price}</i>
 
 📞<i> +998998078071 </i>
+
 📱<a href='https://t.me/iHometashkent'>Телеграм Канал</a> | <a href='https://t.me/Majoor_kz'>Написать 💬</a> 
 #{number_of_rooms}ком
 """
